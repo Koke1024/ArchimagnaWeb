d87c8648da2db2ccc5a20fd250b25bbfde2de348
Matsushita <kmatsushita@g-blossom.com>
2024-11-13 21:32:49 +0900

    パスワード削除

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,10 @@ const knex = require('knex');
 const db = knex({
   client: 'mysql2',
   connection: {
-    host: process.env.REACT_APP_DB_HOST,
-    user: process.env.REACT_APP_DB_USER,
-    password: process.env.REACT_APP_DB_PASSWORD,
-    database: process.env.REACT_APP_DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
 });
 
