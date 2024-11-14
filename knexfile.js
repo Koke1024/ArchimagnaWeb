4997748ca6f76ad76199687ba0b971321648abcd
Matsushita <kmatsushita@g-blossom.com>
2024-11-15 00:51:19 +0900

    env

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,10 @@
-console.dir(process.env);
-
 const db = require('knex')({
   client: 'mysql2',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.REACT_APP_DB_HOST,
+    user: process.env.REACT_APP_DB_USER,
+    password: process.env.REACT_APP_DB_PASSWORD,
+    database: process.env.REACT_APP_DB_NAME,
   },
 });
 
